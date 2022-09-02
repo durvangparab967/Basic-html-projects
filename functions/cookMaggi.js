a425362c26a35f198c79b0405a8daf647a8fd684
durvangparab967 <durvangparab967@gmail.com>
2022-09-02 15:50:49 +0530

    basic function

---
+++ functions/cookMaggi.js
@@ -0,0 +1,8 @@
+function cookMaggi(maggi, water, pot) {
+    console.log(
+      `your maggi will be ready in ${
+        maggi * 2
+      } minutes and the ingredients used are ${maggi} packets of maggi, ${water} ltr of water and ${pot} pot`
+    );
+  }
+  cookMaggi(2,1,1)
