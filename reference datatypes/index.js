faed1a74b286a9e695b3b8f0aa7949bb440dd3bf
durvangparab967 <durvangparab967@gmail.com>
2022-09-07 20:32:58 +0530

    basic object

---
+++ reference datatypes/index.js
@@ -0,0 +1,13 @@
+let student = {
+    name:'Durvang',
+    marks:100,
+    subject:'maths'
+}
+for (let i in student){
+    console.log(i)
+}
+
+let books = ['bk1','bk2','bk3','bk4']
+for(let book of books){
+    console.log(book)
+}  
